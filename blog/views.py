ed9fd4980577e52c7fcf3a039908fd4dc85ea107
kaira96 <kayratsagynbekov@gmail.com>
2019-08-31 13:39:29 +0600

    first commit

---
+++ blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from blog.models import Post
+# Create your views here.
+def get_index(request):
+	posts = Post.objects.all()
+	return render(request, 'index.html', {'posts': posts})
